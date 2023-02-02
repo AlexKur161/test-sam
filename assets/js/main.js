0d85cab241de8bd7d01d44c6d1c5dfccd84b850d
AlexKur161 <kursacha@mail.ru>
2023-02-02 16:29:42 +0300

    /

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
-const circle = document.querySelectorAll('.card-circle')
 const cards = document.querySelectorAll('.card')
 const greenLeaf = document.querySelector('.green-leaf')
 const bordLeaf = document.querySelector('.category-leaf')
+const animation = window.matchMedia('(min-width: 1200px)');
+const circle = document.querySelectorAll('.card-circle')
 
-cards.forEach( itemCards => {
-itemCards.addEventListener('mouseover', () => {
-    greenLeaf.style.left = '-150px'
-    greenLeaf.style.top = '70px'
-    bordLeaf.style.right = '390px'
-    bordLeaf.style.bottom = '85px'
-    })
-    itemCards.addEventListener('mouseout', () => {
-        greenLeaf.style.left = '-160px'
-        greenLeaf.style.top = '80px'
-        bordLeaf.style.right = '400px'
-        bordLeaf.style.bottom = '90px'
+if(animation.matches) {
+    cards.forEach( itemCards => {
+        itemCards.addEventListener('mouseenter', (e) => {
+            greenLeaf.classList.add('green-leaf-ani')
+            bordLeaf.classList.add('bord-leaf-ani')
         })
-})
+        itemCards.addEventListener('mouseleave', (e) => {
+            greenLeaf.classList.remove('green-leaf-ani')
+            bordLeaf.classList.remove('bord-leaf-ani')
+    })
+    })
+    }
